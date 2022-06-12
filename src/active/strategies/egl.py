861e08aee5136008021d812cc87bf13d0be05a3e
Niclas Doll <niclas@amazonis.net>
2022-06-12 23:05:24 +0200

    Implemented Basic Expected Gradient Length Sampling Strategy

---
+++ src/active/strategies/egl.py
@@ -0,0 +1,180 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import ignite.distributed as idist
+# base strategy and utils
+from .strategy import AbstractStrategy
+from .utils import move_to_device
+
+from transformers import PreTrainedModel
+from typing import Tuple, Sequence, Any
+
+class GoodfellowGradientNormForLinear(object):
+    """ Goodfellow Gradient Norm for single linear layer.
+        Attaches hooks to the given linear module. Gradient
+        Norm is ready for computation after backward call.
+
+        Gradient norms are computed for a single batch only
+        and resetted for each forward call to the linear module.
+        However multiple backward calls are supported.
+    
+        Args:
+            module (nn.Linear): linear layer
+    """
+
+    def __init__(self, module:nn.Linear) -> None:
+        assert isinstance(module, nn.Linear)
+        # register capture hooks
+        module.register_forward_hook(self.forward_hook)
+        module.register_full_backward_hook(self.backward_hook)
+        # reset
+        self.H:torch.FloatTensor = None
+        self.Z_bar:torch.FloatTensor = None
+
+    @torch.no_grad()
+    def forward_hook(
+        self,
+        module:nn.Linear,
+        x:Tuple[torch.FloatTensor], 
+        y:Tuple[torch.FloatTensor]
+    ) -> None:
+        """ Forward hook capturing inputs to module 
+        
+            Args:
+                x (Tuple[torch.FloatTensor]): input to linear module
+                y (Tuple[torch.FloatTensor]): output of linear module
+        """
+        # compute squared norm of input
+        self.H = (x[0] * x[0]).flatten(start_dim=1).sum(dim=1)
+        self.Z_bar = []
+
+    @torch.no_grad()
+    def backward_hook(
+        self, 
+        module:nn.Linear,
+        x_grad:Tuple[torch.FloatTensor], 
+        y_grad:Tuple[torch.FloatTensor]
+    ) -> None:
+        """ Backward hook caturing gradients of module outputs 
+        
+            Args:
+                x_grad (Tuple[torch.FloatTensor]): gradient of input
+                y_grad (Tuple[torch.FloatTensor]): gradient of output
+        """
+        # sum of squared gradients
+        z_bar = (y_grad[0] * y_grad[0]).flatten(start_dim=1).sum(dim=1)
+        self.Z_bar.append(z_bar)
+
+    def compute(self) -> torch.FloatTensor:
+        """ Compute Squarred Gradient Norm for last processed batch 
+        
+            Returns:
+                norm (torch.FloatTensor): 
+                    squared gradient norm for weight of linear module
+                    in shape (b, m) where b is the batch size and m is
+                    the number of backward calls.
+        """
+        # concatenate batches
+        H = self.H.unsqueeze(1)
+        Z_bar = torch.stack(self.Z_bar, dim=1)
+        # check dimensions
+        assert H.size(0) == Z_bar.size(0), "Mismatch between forward (%i) and backward (%i) calls" % (H.size(0), Z_bar.size(0))
+        return H * Z_bar
+
+
+class GoodfellowGradientNorm(object):
+    """ Compute Example-wise gradient norm of all linear components in the
+        given module following Goodfellow (2016).
+        
+        Gradient norms are computed for a single batch only
+        and resetted for each forward call to the linear module.
+        However multiple backward calls are supported.
+    
+        Args:
+            module (nn.Module): module which's linear components to analyze
+    """
+
+    def __init__(self, model:nn.Module) -> None:
+        # register capturing hooks
+        self.grad_captures = [
+            GoodfellowGradientNormForLinear(module)
+            for module in model.modules()
+            if isinstance(module, nn.Linear)
+        ]
+
+    def compute(self) -> torch.FloatTensor:
+        """ Compute Squarred Gradient Norm for last processed batch 
+        
+            Returns:
+                norm (torch.FloatTensor): 
+                    squared gradient norm for weight of linear module
+                    in shape (b, m) where b is the batch size and m is
+                    the number of backward calls.
+        """
+        # compute all gradients and concatenate them
+        return sum(grad.compute() for grad in self.grad_captures)
+
+
+class EglForSequenceClassification(AbstractStrategy):
+    """ Implements Maximum Expected Gradient Length Sampling Strategy
+        for sequence classification tasks. Assumes cross-entropy loss.
+    
+        Args:
+            model (PreTrainedModel): 
+                transformer-based sequence classification model
+            k (int):
+                maximum number of classes to consider for
+                expectation approximation. Defaults to 5.
+    """
+
+    def __init__(
+        self,
+        model:PreTrainedModel,
+        k:int =5
+    ) -> None:
+        # initialize strategy
+        super(EglForSequenceClassification, self).__init__()
+        # create gradient norm computer
+        self.grad_norm = GoodfellowGradientNorm(model)
+        # move model to available device(s)
+        self.model = idist.auto_model(model)
+        # save k
+        self.k = k
+
+    def process(self, batch:Any) -> torch.FloatTensor:
+        """ Compute expected gradient lengths for given batch 
+        
+            Args:
+                batch (Any): input batch for which to compute grad norms
+        
+            Returns:
+                norm (torch.FloatTensor): (squared) gradient norm length computed following Goodfellow (2016)
+        """
+        # move batch to device and pass through model
+        batch = move_to_device(batch, device=idist.device())
+        logits = self.model(**batch).logits
+        probs = torch.softmax(logits, dim=-1) 
+        # get top-k predictions
+        assert logits.ndim == 2, "Expected simple sequence classification, i.e. logits of two dimensions but got logits of shape %s" % str(logits.shape)
+        k = min(self.k, logits.size(1))
+        probs, preds = torch.topk(probs, k=k, dim=-1)
+        # compute loss and backpropagate
+        for i in range(k):
+            F.cross_entropy(logits, preds[:, i], reduction='sum') \
+                .backward(retain_graph=True)
+        # approximate expected gradient length
+        with torch.no_grad():
+            return torch.sum(probs * self.grad_norm.compute(), dim=1)
+
+    def sample(self, output:torch.FloatTensor, query_size:int) -> Sequence[int]:
+        """ Get samples with maximum expected gradient length 
+        
+            Args:
+                output (torch.FloatTensor): expected gradient length of samples
+                query_size (int): number of samples to draw
+
+            Returns:
+                indices (Sequence[int]): drawn examples given by their indices in `output`
+        """
+        # get the samples with top expected gradient length
+        return output.topk(k=query_size).indices
--- tests/active/test_strategies.py
+++ tests/active/test_strategies.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import transformers
 # import active learning components
 from src.active.loop import ActiveLoop
@@ -12,6 +13,10 @@ from src.active.strategies.badge import (
     Badge,
     BadgeForSequenceClassification,
     BadgeForTokenClassification
+)
+from src.active.strategies.egl import (
+    GoodfellowGradientNorm,
+    EglForSequenceClassification
 )
 # import helpers
 import pytest
@@ -125,3 +130,92 @@ class TestBadge:
             # check expectation
             if any((message in w.message.args[0]) for w in record):
                 pytest.fail("Unexpected Warning: %s" % message)
+
+
+class TestExpectedGradientLength:
+    """ Test cases for Expected Gradient Length """
+
+    def test_goodfellow_grad_norm(self):
+        """ Test Gradient Norm Computation using Goodfellow """        
+        # create sample model with linear components
+        model = nn.Sequential(
+            nn.Linear(2, 4),
+            nn.Sigmoid(),
+            nn.Linear(4, 4),
+            nn.Sigmoid(),
+            nn.Linear(4, 2)
+        )
+        # some sample input data
+        x = torch.rand(8, 2)
+        y = (torch.rand(8) > 0.5).long()
+
+        # attach gradient norm hooks
+        grad_norm = GoodfellowGradientNorm(model)
+
+        model.zero_grad()
+        # compute gradient norm using goodfellow
+        # use summation to avoid average over batches
+        # this matches the computation with batch size 1
+        F.cross_entropy(model(x), y, reduction='sum').backward()
+        goodfellow_norm = grad_norm.compute()
+
+        # compute norms manually to compare
+        for i in range(x.size(0)):
+            # pass through model
+            model.zero_grad()
+            F.cross_entropy(model(x[i:i+1]), y[i:i+1]).backward()
+            # compute norm
+            manual_norm = sum(
+                (p.grad * p.grad).sum() 
+                for n, p in model.named_parameters() if 'weight' in n
+            )
+            # make sure norms match
+            assert torch.allclose(manual_norm, goodfellow_norm[i]), "%.04f != %.04f" % (manual_norm, goodfellow_norm[i])
+    
+    def test_goodfellow_grad_norm_multiple_backward_passes(self):
+        """ Test multi-backward pass of Goodfellow Norm Computation """
+        # create sample model with linear components
+        model = nn.Sequential(
+            nn.Linear(2, 4),
+            nn.Sigmoid(),
+            nn.Linear(4, 4),
+            nn.Sigmoid(),
+            nn.Linear(4, 2)
+        )
+        # some sample input data
+        x = torch.rand(8, 2)
+
+        # attach gradient norm hooks
+        grad_norm = GoodfellowGradientNorm(model)
+
+        # define reductions
+        # use only summations again to match the
+        # computations done with batch size set to 1
+        # (compare `test_goodfellow_grad_norm`)
+        reductions = [
+            lambda t: t.sum(),
+            lambda t: t.sin().sum(),
+            lambda t: t.exp().sum()
+        ]
+
+        model.zero_grad()
+        # multiple backward passes with different reductions
+        y = model.forward(x)
+        for fn in reductions:
+            fn(y).backward(retain_graph=True)
+        # compute gradient norm using goodfellow
+        goodfellow_norm = grad_norm.compute()
+
+        # compute norms manually to compare
+        for i in range(x.size(0)):
+            for j, fn in enumerate(reductions):
+                # pass through model
+                model.zero_grad()
+                fn(model.forward(x[i:i+1])).backward()
+                # compute norm
+                manual_norm = sum(
+                    (p.grad * p.grad).sum() 
+                    for n, p in model.named_parameters() if 'weight' in n
+                )
+                # make sure norms match
+                assert torch.allclose(manual_norm, goodfellow_norm[i, j]), "%.04f != %.04f" % (manual_norm, goodfellow_norm[i, j])
