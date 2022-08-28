751a70810aa941d203ec151224db618ed857fc66
Niclas Doll <niclas.doll@iais.fraunhofer.de>
2022-08-28 21:55:29 +0200

    Normalize area under learning curve metric and some add some tests for it

--- src/active/metrics/area.py
+++ src/active/metrics/area.py
@@ -2,6 +2,17 @@ from ignite.metrics import Metric
 from scipy.integrate import trapz
 
 class AreaUnderLearningCurve(Metric):
+    """ Compute area under the normalized learning curve.
+        
+        Note that BOTH the x and y values of the curve are
+        normalized to be in range [0, 1]. By this normalization
+        the shape of the curve is more important than the 
+        overall progression during training. (I.e. linear
+        behaviour results in better score than quadratic)
+        
+        Also the normalization guerantees the final score
+        to be in the interval [0, 1].
+    """
 
     def reset(self) -> None:
         # save points on curve
@@ -14,5 +25,10 @@ class AreaUnderLearningCurve(Metric):
         self.y.append(output[1])
 
     def compute(self) -> float:
+        # normalize x values (i.e. steps)
+        # to range from 0 to 1
+        x_max, y_max = max(self.x), max(self.y)
+        x_norm = [x_ / x_max for x_ in self.x]
+        y_norm = [y_ / y_max for y_ in self.y]
         # integrate over curve
-        return trapz(self.y, self.x)
+        return trapz(y_norm, x_norm)
---
+++ tests/active/metrics/test_area.py
@@ -0,0 +1,69 @@
+import numpy as np
+from src.active.metrics import AreaUnderLearningCurve
+
+class TestAreaUnderLearningCurve:
+    """ Test for area under learning curve metric """
+
+    def test_step_normalization(self):
+        # linear sample curve
+        x = np.arange(0, 100, step=5)
+        y = np.linspace(0, 1, x.shape[0])
+        # create metric
+        area = AreaUnderLearningCurve()
+        
+        # reset, update with all pairs defining curve
+        # and compute final metric score
+        area.reset()
+        list(map(area.update, zip(x, y)))
+        valA = area.compute()
+        
+        # reset, update with all pairs defining curve
+        # and compute final metric score
+        area.reset()
+        list(map(area.update, zip(x * 30, y)))
+        valB = area.compute()
+    
+        assert valA == valB
+
+    def test_linear(self):
+        # linear sample curve
+        x = np.arange(0, 100, step=5)
+        y = np.linspace(0, 1, x.shape[0])
+        # create metric
+        area = AreaUnderLearningCurve()
+        # reset, update with all pairs defining curve
+        # and compute final metric score
+        area.reset()
+        list(map(area.update, zip(x, y)))
+        val = area.compute()
+        # should be 0.5 since all linear
+        assert val == 0.5
+        
+    def test_quadratic(self):
+        # quadratic curve
+        x = np.linspace(0, 1, 100)
+        y = x**2
+        # create metric
+        area = AreaUnderLearningCurve()
+        # reset, update with all pairs defining curve
+        # and compute final metric score
+        area.reset()
+        list(map(area.update, zip(x, y)))
+        val = area.compute()
+        # should be < 0.5 since "worse than" linear
+        # note that we are in range [0, 1]
+        assert val < 0.5
+    
+    def test_sqrt(self):
+        # anti-quadratic curve
+        x = np.linspace(0, 1, 100)
+        y = np.sqrt(x)
+        # create metric
+        area = AreaUnderLearningCurve()
+        # reset, update with all pairs defining curve
+        # and compute final metric score
+        area.reset()
+        list(map(area.update, zip(x, y)))
+        val = area.compute()
+        # should be > 0.5 since "better than" linear
+        assert val > 0.5
