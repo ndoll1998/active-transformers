8b218c5574ecaaaabdef2b3fd035ff519e918bdd
Niclas Doll <niclas.doll@iais.fraunhofer.de>
2022-10-11 00:16:24 +0200

    Tests for Train/Validation Data Splitting

--- tests/active/test_engine.py
+++ tests/active/test_engine.py
@@ -34,12 +34,58 @@ class TestActiveLearningEngine:
             ),
             train_val_ratio=1.0 # use all data for training
         )
-
+        
         # create random dataset
         dataset = NamedTensorDataset(
             x=torch.rand(16, 2),
             labels=torch.randint(0, 2, size=(16,))
         )
 
-        # run engine
-        engine.run([dataset])
+        # step engine and check data sizes
+        engine.step(dataset)
+        assert engine.num_train_samples == 16
+        assert engine.num_val_samples == 0
+        
+        # step engine and check data sizes
+        engine.step(dataset)
+        assert engine.num_train_samples == 32
+        assert engine.num_val_samples == 0
+
+    def test_populate_train_and_val_data(self):
+        
+        # register classification model    
+        register_classification_model() 
+
+        # create model and optimizer
+        model = ClassificationModel(ClassificationModelConfig(2, 2))
+        optim = torch.optim.SGD(model.parameters(), lr=0.01)
+
+        # create al engine
+        engine = ActiveLearningEngine(
+            trainer=Trainer(model, optim, incremental=True),
+            trainer_run_kwargs=dict(
+                max_epochs=5
+            ),
+            train_val_ratio=0.8 # use 80% for training
+        )
+
+        # create random dataset
+        dataset = NamedTensorDataset(
+            x=torch.rand(16, 2),
+            labels=torch.randint(0, 2, size=(16,))
+        )
+        
+        # step engine and check data sizes
+        engine.step(dataset)
+        assert engine.num_train_samples == 13
+        assert engine.num_val_samples == 3        
+
+        # step engine and check data sizes
+        engine.step(dataset)
+        assert engine.num_train_samples == 26
+        assert engine.num_val_samples == 6
+        
+        # step engine and check data sizes
+        engine.step(dataset)
+        assert engine.num_train_samples == 38
+        assert engine.num_val_samples == 10
