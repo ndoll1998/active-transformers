9e3ab95fabca690699ab93ec8c9f0259a3d9970c
Niclas Doll <niclas.doll@iais.fraunhofer.de>
2022-10-19 23:33:19 +0200

    Please just work

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="active-transformers",
-    version="0.0.3",
+    version="0.0.4",
     description="Active Learning for Transformer with focus on Sequence Tagging tasks",
     long_description=open("README.md", "r").read(),
     long_description_content_type='text/markdown',
